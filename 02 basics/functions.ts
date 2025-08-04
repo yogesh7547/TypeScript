d21c3c05d26b158346f7d8d97e50601f25398e16
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-08-04 15:55:13 +0530

    good practices for writing functions with TS

--- 02 basics/functions.ts
+++ 02 basics/functions.ts
@@ -1,5 +1,6 @@
-function addTwo(num:number){
+function addTwo(num:number):number{
     return num+2;
+    // return "hello";//you cant return string since return type is mentioned
 }
 
 
@@ -13,6 +14,33 @@ function signupUser(name:string, email:string, ispaid:boolean){
 }
 
 let loginUser=(name:string, email:string, ispaid:boolean=false)=>{}
+
+/* function getValue(myVal:number){
+    if(myVal>5){
+        return true;
+    }
+    return "200 OK";
+}
+ */
+
+const getHello=(s:string):string=>{
+    return "";
+}
+
+const heros=["thor", "ironman","batman"]
+// const heros=[1,2,3]
+
+heros.map((hero):string=>{
+    return `hero is ${hero}`
+})
+
+function consoleError(errMsg:string):void{
+    console.log(errMsg);
+}
+
+function handleError(errMsg:string):never{
+    throw new Error(errMsg)
+}
 
 addTwo(5);
 getUpper("TREu")
