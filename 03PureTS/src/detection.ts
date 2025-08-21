171700e9497b097c92c6a5344d6e9892975f8c9d
yogesh7547 <yogeshkurmalker7@gmail.com>
2025-08-21 12:28:28 +0530

    discrimated union and exhaustiveness checking with Never

--- 03PureTS/src/detection.ts
+++ 03PureTS/src/detection.ts
@@ -64,3 +64,51 @@ function getFood(pet:Fish|Bird){
          return "bird food"
     }
 }
+
+
+interface Circle{
+    kind:"circle",
+    radius:number
+}
+
+interface Square{
+    kind:"square",
+    side:number
+}
+
+
+
+interface Rectangle{
+    kind:"rectangle",
+    length:number,
+    width:number
+}
+
+type Shape= Circle|Square|Rectangle
+
+function getTrueShape(shape:Shape){
+    if(shape.kind==="circle"){
+        return Math.PI * shape.radius ** 2
+    }
+    // return shape.side *shape.side
+}
+
+function getArea(shape:Shape){
+    switch(shape.kind){
+        case "circle":
+           return Math.PI * shape.radius ** 2
+
+        case "square":
+             return shape.side *shape.side 
+
+        case "rectangle":
+            return shape.length *shape.width
+
+
+        default:
+            const _defaultforshape: never = shape  
+            return _defaultforshape   
+
+    }   
+      
+}
